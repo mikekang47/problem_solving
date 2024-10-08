645724f9d00f7fc6a28d83a0fcb90ec3713f2263
sihoo kang <sihookang47@gmail.com>
2024-10-08 23:56:10 +0900

    [Bronze III] Title: 직각삼각형, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    numbers = list(map(int, input().split()))
+    numbers.sort()
+
+    a, b, c = numbers[0], numbers[1], numbers[2]
+
+    if a == 0 and b == 0 and c == 0:
+        exit(0)
+
+    if c**2 == a**2 + b**2:
+        print("right")
+    else:
+        print("wrong")
